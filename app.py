c077e63a432c8c4cfa83f9f6d1faa9b89c837ee5
kamlesh Bagdawat <kamleshbagdawat93@gmail.com>
2026-05-29 21:31:19 +0530

    fix st.image(img, use_container_width=True)

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def poster_grid(cards, cols=6, key_prefix="grid"):
 
             with colset[c]:
                 if poster:
-                    st.image(poster, use_column_width=True)
+                    st.image(img, use_container_width=True)
                 else:
                     st.write("🖼️ No poster")
 
